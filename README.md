f6f388385d1c23ccb7fb59283efcf7beb7ed2991
grayfallstown <grayfallstown@protonmail.com>
2023-01-04 19:30:36 +0100

    first working version

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-# [pâro](https://www.dictionaryofobscuresorrows.com/post/173924002125/p%C3%A2ro-n-the-feeling-that-no-matter-what-you-do-is)-rs
+# pâro-rs
 
-An opinionated way to develop tauri desktop applications where you do not write any client code (no javascript, no webassembly) as all html rendering and event handling is done inside your tauri application and pâro has the minimal client code that is still required already done for you.
+An opinionated way to develop [tauri](https://tauri.app/) desktop applications powered by html and csss where you do not write any client code (no javascript, no webassembly) as all html rendering and event handling is done inside your tauri application. pâro has the minimal client code that is still required already done for you. No more https calls or dealing with JSON.
 
-## the pâro library is still in development and I will publish the library as soon as I got a complete prototype
+This cuts down on code complexity, build process complexity, compile time, nerve-wracking and frustration.
+
+pâro does not enforce how you generate your html. Use your favorite template engine or just use format!() on strings. pâro does not care, but [maud compile time templates](https://github.com/lambda-fairy/maud) will be used in many examples to get compile time checks.
+
+# the pâro library is currently in pre-alpha state and will have small api changes soon.
+
+## Where the name comes from
+
+> The name pâro comes from the [Dictionary of obscure sorrows](https://www.dictionaryofobscuresorrows.com/post/173924002125/p%C3%A2ro-n-the-feeling-that-no-matter-what-you-do-is) and describes the feeling that no matter what you do is always somehow wrong—that any attempt to make your way comfortably through the world will only end up crossing some invisible taboo—as if there’s some obvious way forward that everybody else can see but you, each of them leaning back in their chair and calling out helpfully, colder, colder, colder.
+
+That was what I felt writing my first tauri app and having to write an entire second application for the GUI, separated by http calls and json (de-)serialization, as well as during pâro's conceptual phase.
+
+
+
+## Roadmap:
+
+- [x] Get pâro working
+- [ ] Improve API (CallbackStore vs ApplicationState as toplevel element)
+- [ ] Examples
+  - [ ] Minimal counter example
+  - [ ] Implement GUI for [html2maud](https://github.com/grayfallstown/html2maud) with pâro and put it as submodule under examples
+  - [ ] More complex example with
+    - [ ] Routing
+    - [ ] Conditional rendering
+    - [ ] Server side non gui events like api events or async database queries
+- [ ] pâro starter
+    - [ ] As github starter repository
+    - [ ] Include basics as routing, a formular and composing
+    - [ ] pure format!() pâro starter
+    - [ ] maud pâro starter
+- [ ] Dokumentation
+- [ ] Establish Best Practices
+- [ ] Differential html update
+- [ ] Get a Logo
---
+++ src/lib.rs
@@ -0,0 +1,90 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+/**
+ * The heart of pâro. A server side (as in, inside your tauri application)
+ * callback store that holds all your server side callbacks / eventhandlers.
+ */
+ #[derive(Clone)]
+pub struct CallbackStore {
+    callbacks: HashMap<String, Arc<Mutex<dyn FnMut() + Send + 'static>>>
+}
+
+impl CallbackStore {
+    pub fn new() -> CallbackStore {
+        CallbackStore {
+            callbacks: HashMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, id: String, callback: Arc<Mutex<dyn FnMut() + Send + 'static>>) -> () {
+        if (self.callbacks.contains_key(&id)) {
+            panic!("[paro] callback ids must be unique, '{}' is not", &id);
+        }
+        self.callbacks.insert(id, callback);
+    }
+
+    pub fn clear(&mut self) -> () {
+        self.callbacks.clear();
+    }
+
+    pub fn call(&mut self, id: String) -> Result<(), String> {
+        match self.callbacks.get(&id) {
+            Some(callback) => {
+                let mut locked = { callback.lock().unwrap() };
+                Ok(locked())
+            },
+            None => Err(format!("[paro] callback '{}' not found", &id))
+        }
+    }
+}
+
+/***
+ * Creates a mew event / callback that you can reference in your html.
+ * The event / callback is executed inside your tauri app / the server
+ * and event! returns a minimal javascript call to tell pâro which
+ * event / callback should be triggered on the server side. What you
+ * write is pure rust, pâro handles javscript.
+ * 
+ * Example usage without maud templates
+ * let counterBtnMarkup = format!(r#"<button onclick="{}">
+ *         counter: {}
+ *     </button>"#, event!(state, (move |mut state| {
+ *               let mut callback_state = state.lock().unwrap();
+ *               callback_state.numbers[0] = callback_state.numbers[0] + 1;
+ *               println!("first number of state.numbers updated to: {}", callback_state.numbers[0]);
+ *           })), state.lock().unwrap().numbers[0]);
+ * 
+ * Example usage with maud templates:
+ * 
+ *   let counterBtnMarkup = html! {
+ *       button onclick=({
+ *           event!(state, (move |mut state| {
+ *               let mut callback_state = state.lock().unwrap();
+ *               callback_state.numbers[0] = callback_state.numbers[0] + 1;
+ *               println!("first number of state.numbers updated to: {}", callback_state.numbers[0]);
+ *           }))
+ *       }) { "counter:" (state.lock().unwrap().numbers[0]) }
+ *   };
+ */
+#[macro_export]
+macro_rules! event {
+    ($state:expr, $closure:tt)=>{
+        {
+            let mut state_clone = $state.clone();
+            let callback_id = uuid::Uuid::new_v4().to_string();
+            let callback = move || {
+                let mut closure_box: Box<dyn FnMut(&mut Arc<Mutex<ApplicationState>>) + Send + 'static> = Box::new($closure);
+                closure_box(&mut state_clone);
+            };
+            {
+                $state.lock().unwrap().callback_store.insert(
+                    callback_id.clone(),
+                    Arc::new(Mutex::new(callback))
+                );
+            }
+            let javascript_call = format!{"window.__PARO__.emitEvent(\"{}\")", &callback_id};
+            javascript_call
+        }
+    }
+}
